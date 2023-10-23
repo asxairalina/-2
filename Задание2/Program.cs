2f68daa3ffb29db736dceab7b6b5f9e693dc8ca9
212227 <212227@1220-012-22>
2023-10-23 10:35:03 +0300

    Добавьте файлы проекта.

---
+++ Задание2/Program.cs
@@ -0,0 +1,24 @@
+﻿int a;
+int b;
+int c;
+double v1;
+double v2;
+double v3;
+
+while (true)
+{
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    c = Convert.ToInt32(Console.ReadLine());
+    if (a < 100000 && b < 100000 && c < 100000)
+    {
+         v1 = (double) a / 2;
+        Console.WriteLine(v1);
+         v2 = (double) b / 2;
+        Console.WriteLine(v2);
+        v3 = (double) c / 2;
+        Console.WriteLine(v3);
+        double d = Math.Ceiling(v1) + Math.Ceiling(v2) + Math.Ceiling(v3);
+        Console.WriteLine(d);
+    }
+}
